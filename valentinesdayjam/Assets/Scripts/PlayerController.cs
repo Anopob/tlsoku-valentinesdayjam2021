04119f161a8a2ae26788be04f44c10c7277a4827
Neil Warnock <Neil@ecere.com>
2021-02-11 18:00:01 -0500

    Player jumping only when on ground,  fixed player groundedness

--- valentinesdayjam/Assets/Scripts/PlayerController.cs
+++ valentinesdayjam/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
+
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -10,16 +12,20 @@ public class PlayerController : MonoBehaviour
     public float jumpSpeed = 2;
     
     private bool facingLeft = false;
-    
+    private bool jumping = false;
+
     private Animator anim;
     private Rigidbody2D rb;
     private SpriteRenderer sp;
+    private BoxCollider2D playerCollider;
+    
     // Start is called before the first frame update
     void Start()
     {        
         anim = GetComponent<Animator> ();
 		rb = GetComponent<Rigidbody2D> ();
         sp = GetComponent<SpriteRenderer> ();
+        playerCollider = Array.Find(GetComponents<BoxCollider2D>(), b => !b.isTrigger);
    }
 
     // Update is called once per frame
@@ -45,9 +51,10 @@ public class PlayerController : MonoBehaviour
         rb.velocity = new Vector2 (newHorizontalVelocity, rb.velocity.y);
         anim.SetFloat("speed", Mathf.Abs(rb.velocity.x));
     
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && !jumping)
         {
-            PerformJump();
+            PerformJumpingAnimations();
+            rb.velocity = new Vector2 (rb.velocity.x, jumpSpeed);
         }
     }
     
@@ -57,10 +64,30 @@ public class PlayerController : MonoBehaviour
         sp.flipX = facingLeft;
     }
     
-    void PerformJump()
+    void PerformJumpingAnimations()
     {
+        anim.ResetTrigger("landed");
         anim.SetTrigger("jumped");
-        rb.velocity = new Vector2 (rb.velocity.x, jumpSpeed);
+        jumping = true;
     }
     
+    void PerformLandingAnimations()
+    {
+        jumping = false;
+        anim.ResetTrigger("jumped");
+        anim.SetTrigger("landed");
+    }
+ 
+    void OnTriggerStay2D(Collider2D other)
+    {
+        Debug.Log(other);
+        if (other != playerCollider)
+            PerformLandingAnimations();
+    }
+    
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other != playerCollider)
+            PerformJumpingAnimations();
+    }
 }
