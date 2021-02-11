733f78209914404b234cd229234ab912598789f8
Anopob <anopob@hotmail.com>
2021-02-11 18:42:18 -0500

    Fix Player jump

--- valentinesdayjam/Assets/Scripts/PlayerController.cs
+++ valentinesdayjam/Assets/Scripts/PlayerController.cs
@@ -18,73 +18,75 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private SpriteRenderer sp;
     private BoxCollider2D playerCollider;
-    
+
     // Start is called before the first frame update
     void Start()
-    {        
-        anim = GetComponent<Animator> ();
-		rb = GetComponent<Rigidbody2D> ();
-        sp = GetComponent<SpriteRenderer> ();
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sp = GetComponent<SpriteRenderer>();
         playerCollider = Array.Find(GetComponents<BoxCollider2D>(), b => !b.isTrigger);
-   }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !jumping)
+        {
+            PerformJumpingAnimations();
+            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+    }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+
         float horizontal = Input.GetAxis("Horizontal");
         float newHorizontalVelocity;
-        
+
         if (horizontal == 0)
             newHorizontalVelocity = 0;
         else if (horizontal < 0)
             newHorizontalVelocity = Mathf.Max(maxWalkSpeed * -1, rb.velocity.x + horizontal * acceleration);
         else
             newHorizontalVelocity = Mathf.Min(maxWalkSpeed, rb.velocity.x + horizontal * acceleration);
-            
-        
+
         if ((newHorizontalVelocity < 0) != facingLeft)
         {
             FlipSprite();
         }
-        
-        rb.velocity = new Vector2 (newHorizontalVelocity, rb.velocity.y);
+
+        rb.velocity = new Vector2(newHorizontalVelocity, rb.velocity.y);
         anim.SetFloat("speed", Mathf.Abs(rb.velocity.x));
-    
-        if (Input.GetButtonDown("Jump") && !jumping)
-        {
-            PerformJumpingAnimations();
-            rb.velocity = new Vector2 (rb.velocity.x, jumpSpeed);
-        }
     }
-    
+
     void FlipSprite()
     {
         facingLeft = !facingLeft;
         sp.flipX = facingLeft;
     }
-    
+
     void PerformJumpingAnimations()
     {
         anim.ResetTrigger("landed");
         anim.SetTrigger("jumped");
         jumping = true;
     }
-    
+
     void PerformLandingAnimations()
     {
         jumping = false;
         anim.ResetTrigger("jumped");
         anim.SetTrigger("landed");
     }
- 
+
     void OnTriggerStay2D(Collider2D other)
     {
         Debug.Log(other);
         if (other != playerCollider)
             PerformLandingAnimations();
     }
-    
+
     void OnTriggerExit2D(Collider2D other)
     {
         if (other != playerCollider)
