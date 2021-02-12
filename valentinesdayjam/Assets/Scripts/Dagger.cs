0961a3520529806218a9806c06427dbff7ec6835
Neil Warnock <Neil@ecere.com>
2021-02-11 21:17:17 -0500

    Dagger trap that flies in a straight line

---
+++ valentinesdayjam/Assets/Scripts/Dagger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dagger : MonoBehaviour
+{
+    public float speed;
+    public float angle;
+    
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Transform>().rotation = Quaternion.Euler(0.0f, 0.0f,  angle - 90.0f);
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad) * -1, Mathf.Sin(angle  *  Mathf.Deg2Rad)) * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
