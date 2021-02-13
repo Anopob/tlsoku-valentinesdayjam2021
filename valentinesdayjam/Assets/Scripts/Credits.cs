def6003a1acfd2e3316754f4ca008e2b0e396148
Anopob <anopob@hotmail.com>
2021-02-13 18:44:03 -0500

    Add initial credits to MainMenu

---
+++ valentinesdayjam/Assets/Scripts/Credits.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Credits : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _creditsPanel, _mainPanel;
+
+    public void TurnOnCredits()
+    {
+        _creditsPanel.SetActive(true);
+        _mainPanel.SetActive(false);
+    }
+
+    public void TurnOffCredits()
+    {
+        _creditsPanel.SetActive(false);
+        _mainPanel.SetActive(true);
+    }
+}
