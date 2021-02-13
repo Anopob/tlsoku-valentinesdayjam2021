ebf485d217c01a54f09c691c572c6d178f84bf3b
Anopob <anopob@hotmail.com>
2021-02-13 16:06:27 -0500

    Add MainMenu easter egg

---
+++ valentinesdayjam/Assets/MainMenuEasterEgg.cs
@@ -0,0 +1,16 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuEasterEgg : MonoBehaviour
+{
+    private SceneCalculator _sceneCalculator;
+
+    private void Start()
+    {
+        _sceneCalculator = FindObjectOfType<SceneCalculator>();
+        if (_sceneCalculator.HasBeatenGame)
+            GetComponent<SpriteRenderer>().enabled = true;
+    }
+}
